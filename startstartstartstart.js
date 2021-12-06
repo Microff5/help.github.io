cd529ec8729b0085d1ffd40235810d4f4f5b6695
Microff5 <95642455+Microff5@users.noreply.github.com>
2021-12-06 11:13:18 -0600

    Create startstartstartstart.js

---
+++ startstartstartstart.js
@@ -0,0 +1,11 @@
+function startstart(){
+  var distance = 0
+  var velocity = 0
+  var acceleration = 0
+  var jerk = 0
+  var snap = 0
+  var crackle = 0
+  var pop = 0
+  var zoom = 0
+  var timespeed = 0
+}
